7a1dbd0bb7c8429929dbd62580c65657c157b40a
Resurrect <resurrectdev@protonmail.com>
2026-05-18 19:17:02 -0400

    Implement widget update functionality in MainActivity
    Added a method channel to handle widget updates and refresh all widgets.

--- android/app/src/main/kotlin/com/resurrect/grove/MainActivity.kt
+++ android/app/src/main/kotlin/com/resurrect/grove/MainActivity.kt
@@ -1,8 +1,43 @@
 package com.resurrect.grove
 
-// We change FlutterActivity to FlutterFragmentActivity to support local_auth
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.content.Intent
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterFragmentActivity() {
-    // This allows the biometric dialog to anchor itself to the app life cycle
+class MainActivity : FlutterFragmentActivity() {
+
+    private val CHANNEL = "com.grove.app/widgets"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+        .setMethodCallHandler { call, result ->
+            if (call.method == "updateWidgets") {
+                refreshAllWidgets()
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun refreshAllWidgets() {
+        val mgr = AppWidgetManager.getInstance(this)
+        listOf(
+            CalendarWidgetProvider::class.java,
+            TreeWidgetProvider::class.java
+        ).forEach { cls ->
+            val ids = mgr.getAppWidgetIds(ComponentName(this, cls))
+            if (ids.isNotEmpty()) {
+                sendBroadcast(Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE).apply {
+                    component = ComponentName(this@MainActivity, cls)
+                    putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
+                })
+            }
+        }
+    }
 }
