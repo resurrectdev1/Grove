57d0364099444505444fd65a959957233b6ff907
Resurrect <resurrectdev@protonmail.com>
2026-05-24 15:55:02 -0400

    Refactor SharedPreferences usage in grove_settings.dart

--- lib/providers/grove_settings.dart
+++ lib/providers/grove_settings.dart
@@ -12,6 +12,8 @@ class GroveSettings extends ChangeNotifier {
   bool           _biometricUnlock   = false;
   TimeOfDay      _notifTime         = const TimeOfDay(hour: 20, minute: 0);
 
+  SharedPreferences? _prefs;
+
   GroveThemeMode get themeMode         => _themeMode;
   LayoutMode     get layoutMode        => _layoutMode;
   bool           get onboardingDone    => _onboardingDone;
@@ -21,14 +23,14 @@ class GroveSettings extends ChangeNotifier {
   GroveTheme     get theme             => GroveTheme(mode: _themeMode, dynamicScheme: _dynamicScheme);
 
   Future<void> init(ColorScheme? dynamicLight, ColorScheme? dynamicDark) async {
-    final prefs       = await SharedPreferences.getInstance();
-    final savedTheme  = prefs.getInt('theme_mode')   ?? 0;
-    final savedLayout = prefs.getInt('layout_mode')  ?? 0;
-    final savedHour   = prefs.getInt('notif_hour')   ?? 20;
-    final savedMinute = prefs.getInt('notif_minute') ?? 0;
-    _onboardingDone    = prefs.getBool('onboarding_done')    ?? false;
-    _dailyNotification = prefs.getBool('daily_notification') ?? false;
-    _biometricUnlock   = prefs.getBool('biometric_unlock')   ?? false;
+    _prefs = await SharedPreferences.getInstance();
+    final savedTheme  = _prefs!.getInt('theme_mode')   ?? 0;
+    final savedLayout = _prefs!.getInt('layout_mode')  ?? 0;
+    final savedHour   = _prefs!.getInt('notif_hour')   ?? 20;
+    final savedMinute = _prefs!.getInt('notif_minute') ?? 0;
+    _onboardingDone    = _prefs!.getBool('onboarding_done')    ?? false;
+    _dailyNotification = _prefs!.getBool('daily_notification') ?? false;
+    _biometricUnlock   = _prefs!.getBool('biometric_unlock')   ?? false;
     _notifTime         = TimeOfDay(hour: savedHour, minute: savedMinute);
     if (savedTheme  < GroveThemeMode.values.length) _themeMode  = GroveThemeMode.values[savedTheme];
     if (savedLayout < LayoutMode.values.length)     _layoutMode = LayoutMode.values[savedLayout];
@@ -43,8 +45,7 @@ class GroveSettings extends ChangeNotifier {
 
   Future<void> setDailyNotification(bool value) async {
     _dailyNotification = value;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('daily_notification', value);
+    await _prefs?.setBool('daily_notification', value);
     if (value) {
       await GroveNotifications.instance.scheduleDailyReminder(time: _notifTime);
     } else {
@@ -55,9 +56,8 @@ class GroveSettings extends ChangeNotifier {
 
   Future<void> setNotifTime(TimeOfDay time) async {
     _notifTime = time;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt('notif_hour',   time.hour);
-    await prefs.setInt('notif_minute', time.minute);
+    await _prefs?.setInt('notif_hour',   time.hour);
+    await _prefs?.setInt('notif_minute', time.minute);
     if (_dailyNotification) {
       await GroveNotifications.instance.scheduleDailyReminder(time: time);
     }
@@ -66,29 +66,25 @@ class GroveSettings extends ChangeNotifier {
 
   Future<void> setBiometricUnlock(bool value) async {
     _biometricUnlock = value;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('biometric_unlock', value);
+    await _prefs?.setBool('biometric_unlock', value);
     notifyListeners();
   }
 
   Future<void> completeOnboarding() async {
     _onboardingDone = true;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('onboarding_done', true);
+    await _prefs?.setBool('onboarding_done', true);
     notifyListeners();
   }
 
   Future<void> setThemeMode(GroveThemeMode mode) async {
     _themeMode = mode;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt('theme_mode', mode.index);
+    await _prefs?.setInt('theme_mode', mode.index);
     notifyListeners();
   }
 
   Future<void> setLayoutMode(LayoutMode mode) async {
     _layoutMode = mode;
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setInt('layout_mode', mode.index);
+    await _prefs?.setInt('layout_mode', mode.index);
     notifyListeners();
   }
 }
