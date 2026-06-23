e0713dd9d9d78121e6dbd6d30bec90b71fa15571
Resurrect <resurrectdev@protonmail.com>
2026-06-22 22:33:54 -0400

    Implement daily reminder and custom accent features
    Added daily reminder functionality with custom accent support.

--- lib/providers/grove_settings.dart
+++ lib/providers/grove_settings.dart
@@ -10,18 +10,24 @@ class GroveSettings extends ChangeNotifier {
   bool           _onboardingDone         = false;
   bool           _biometricUnlock        = false;
   bool           _milestoneNotifications = false;
+  bool           _dailyReminder          = false;
+  TimeOfDay?     _dailyReminderTime;
+  Color?         _customAccent;
   Locale?        _locale;
 
   SharedPreferences? _prefs;
 
-  GroveThemeMode get themeMode             => _themeMode;
-  LayoutMode     get layoutMode            => _layoutMode;
-  bool           get onboardingDone        => _onboardingDone;
-  bool           get biometricUnlock       => _biometricUnlock;
+  GroveThemeMode get themeMode              => _themeMode;
+  LayoutMode     get layoutMode             => _layoutMode;
+  bool           get onboardingDone         => _onboardingDone;
+  bool           get biometricUnlock        => _biometricUnlock;
   bool           get milestoneNotifications => _milestoneNotifications;
-  GroveTheme     get theme                 => GroveTheme(mode: _themeMode, dynamicScheme: _dynamicScheme);
+  bool           get dailyReminder          => _dailyReminder;
+  TimeOfDay?     get dailyReminderTime      => _dailyReminderTime;
+  Color?         get customAccent           => _customAccent;
+  GroveTheme     get theme                  => GroveTheme(mode: _themeMode, dynamicScheme: _dynamicScheme, customAccent: _customAccent);
 
-  Locale?        get locale               => _locale;
+  Locale?        get locale                => _locale;
 
   Future<void> init(ColorScheme? dynamicLight, ColorScheme? dynamicDark) async {
     _prefs = await SharedPreferences.getInstance();
@@ -31,6 +37,16 @@ class GroveSettings extends ChangeNotifier {
     _onboardingDone         = _prefs!.getBool('onboarding_done')         ?? false;
     _biometricUnlock        = _prefs!.getBool('biometric_unlock')        ?? false;
     _milestoneNotifications = _prefs!.getBool('milestone_notifications') ?? false;
+    _dailyReminder          = _prefs!.getBool('daily_reminder')          ?? false;
+
+    final reminderHour   = _prefs!.getInt('daily_reminder_hour');
+    final reminderMinute = _prefs!.getInt('daily_reminder_minute');
+    if (reminderHour != null && reminderMinute != null) {
+      _dailyReminderTime = TimeOfDay(hour: reminderHour, minute: reminderMinute);
+    }
+
+    final accentInt = _prefs!.getInt('custom_accent');
+    if (accentInt != null) _customAccent = Color(accentInt);
 
     if (savedTheme  < GroveThemeMode.values.length) _themeMode  = GroveThemeMode.values[savedTheme];
     if (savedLayout < LayoutMode.values.length)     _layoutMode = LayoutMode.values[savedLayout];
@@ -82,6 +98,32 @@ class GroveSettings extends ChangeNotifier {
   Future<void> setLayoutMode(LayoutMode mode) async {
     _layoutMode = mode;
     await _prefs?.setInt('layout_mode', mode.index);
+    notifyListeners();
+  }
+
+  Future<void> setCustomAccent(Color? color) async {
+    _customAccent = color;
+    if (color == null) {
+      await _prefs?.remove('custom_accent');
+    } else {
+      await _prefs?.setInt('custom_accent', color.toARGB32());
+    }
+    notifyListeners();
+  }
+
+  Future<void> setDailyReminder(bool enabled, TimeOfDay? time) async {
+    _dailyReminder     = enabled;
+    _dailyReminderTime = enabled ? time : null;
+    await _prefs?.setBool('daily_reminder', enabled);
+    if (enabled && time != null) {
+      await _prefs?.setInt('daily_reminder_hour',   time.hour);
+      await _prefs?.setInt('daily_reminder_minute', time.minute);
+      await GroveNotifications.instance.scheduleDailyReminder(time);
+    } else {
+      await _prefs?.remove('daily_reminder_hour');
+      await _prefs?.remove('daily_reminder_minute');
+      await GroveNotifications.instance.cancelDailyReminder();
+    }
     notifyListeners();
   }
 
