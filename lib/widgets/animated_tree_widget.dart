a6d4743e1e91034fbc55f8b810b0dea3e8696042
Resurrect <resurrectdev@protonmail.com>
2026-06-26 14:10:38 -0400

    Refactor AnimatedTreeWidget for burst animation support

--- lib/widgets/animated_tree_widget.dart
+++ lib/widgets/animated_tree_widget.dart
@@ -10,29 +10,48 @@ class AnimatedTreeWidget extends StatefulWidget {
 }
 
 class _AnimatedTreeWidgetState extends State<AnimatedTreeWidget>
-with SingleTickerProviderStateMixin {
+with TickerProviderStateMixin {
   late AnimationController _windController;
+  late AnimationController _burstController;
+  late GrowthStage _lastStage;
 
   @override
   void initState() {
     super.initState();
+    _lastStage = widget.habit.stage;
     _windController = AnimationController(vsync: this, duration: const Duration(seconds: 6))..repeat();
+    _burstController = AnimationController(vsync: this, duration: const Duration(milliseconds: 1600));
   }
 
   @override
-  void dispose() { _windController.dispose(); super.dispose(); }
+  void didUpdateWidget(AnimatedTreeWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final newStage = widget.habit.stage;
+    if (newStage != _lastStage && newStage.index > _lastStage.index) {
+      _burstController.forward(from: 0);
+    }
+    _lastStage = newStage;
+  }
+
+  @override
+  void dispose() {
+    _windController.dispose();
+    _burstController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) => AnimatedBuilder(
-    animation: _windController,
+    animation: Listenable.merge([_windController, _burstController]),
     builder: (_, _) => CustomPaint(
       painter: FractalTreePainter(
-        stage:       widget.habit.stage,
-        baseColor:   widget.habit.color,
-        progress:    widget.habit.stageProgress,
-        windPhase:   _windController.value * 2 * math.pi,
-        daysElapsed: widget.habit.daysElapsed,
-        geneticSeed: widget.habit.geneticSeed,
+        stage:         widget.habit.stage,
+        baseColor:     widget.habit.color,
+        progress:      widget.habit.stageProgress,
+        windPhase:     _windController.value * 2 * math.pi,
+        daysElapsed:   widget.habit.daysElapsed,
+        geneticSeed:   widget.habit.geneticSeed,
+        burstProgress: _burstController.value,
       ),
     ),
   );
